5115fbcc8adb0aa120120b51533278a51a45ee54
ga1ker <zenget008@gmail.com>
2025-10-06 12:41:33 -0600

    inicializar pwa

---
+++ sw.js
@@ -0,0 +1,61 @@
+// Plantilla básica para un service worker;
+
+
+// Nombre del sw y los archivos a cachear;
+const CACHE_NAME = "aromantial"
+const urlsToCache = [
+    'index.html',
+    'style.css',
+    'app.js',
+    'offline.html',
+    'icons/icon-192x192.png',
+    'icons/icon-512x512.png',
+];
+
+// 2. INSTALL -> Se ejecuta al intstalar el service worker;
+// se cachean los recursos base de la PWA;
+
+self.addEventListener("install", event => {
+    console.log("Service worker instalandose")
+    event.waitUntil(
+        caches.open((CACHE_NAME).then(cache => {
+            cache.addAll(urlsToCache)
+            console.log("archivos en cache, ", urlsToCache)
+        }))
+    );
+});
+
+// 3. Activate => Se ejecuta al activar el service worker;
+// borrar caché viejo para solo mantener la más reciente;
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => 
+            Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                .map(key => caches.delete(key))
+            )
+        )
+    );
+});
+
+// 4. Fetch intercepta las peticiones de la app web
+// y responde con los recursos cacheados si están disponibles;
+// Si no, intenta hacer la petición a la red;
+// en caso de falla muestra la página offline;
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request).catch(() => caches.match('offline.html'))
+        })
+    )
+})
+
+// 5. Push => Notificaciones en segundo plano
+// manejo de notificaciones push opcional
+self.addEventListener("push", event => {
+    const data = event.data ? event.data.text() : "Notificación sin texto";
+    event.waitUntil(
+        self.ServiceWorkerRegistration.showNotification("Mi PWA", {body: data})
+    )
+})
